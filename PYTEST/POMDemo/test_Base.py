f7d61918becd1e5f178790066ddf06112cb2163d
Vineethac96 <41900084+Vineethac96@users.noreply.github.com>
2022-11-16 17:41:17 +0100

    Delete test_Base.py

--- PYTEST/POMDemo/test_Base.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-'''init__driver fixture is applied on base class and this class is inherited by other login and home test class'''
-@pytest.mark.usefixtures("init__driver")
-class BaseTest:
-    pass
