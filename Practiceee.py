04bcfa7a4cd2a1207a0852526256f0875662d652
Vineethac96 <vineethac96@gmail.com>
2023-08-10 15:06:23 +0200

    added

---
+++ Practiceee.py
@@ -0,0 +1,20 @@
+name = "Vineetha"
+# print(name)
+# print(name.__doc__)
+
+def calc(num1,num2,op):
+
+        if op == "+":
+            res = num1+num2
+        elif op == "-":
+            res = num1 - num2
+        elif op == "*":
+            res = num1*num2
+        elif op == "/":
+            res = num1/num2
+        return res
+
+num1 = float(input("enter 1st munbers\n"))
+num2 = float(input("enter 2nd munbers\n"))
+op = input("enter operator\n")
+print(calc(num1,num2,op))
